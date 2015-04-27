a1a771c7bbb5250106edef3c65d1c1569a4cb2e1
Josh Strupp <joshstrupp@gmail.com>
2015-04-27 14:50:07 -0400

    final project update with csv

---
+++ final_project/titans.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 14:27:48 2015
+
+@author: joshstrupp
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+drinks = pd.read_csv('nfl2000stats.csv', sep=',')
